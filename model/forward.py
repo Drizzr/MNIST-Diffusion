ea869dd35ae23bd8178bb997c32852cc1f880e22
Samuel Friese <samuel.friese@tum.de>
2024-10-07 12:55:23 +0200

    -

--- model/forward.py
+++ model/forward.py
@@ -43,7 +43,7 @@ class ForwardDiffusion:
 
         If t.shape[0] != vals.shape[0], the function will return the sample for the corresponding index
         """
-        vals.to(self.device)
+        vals = vals.to(self.device)
         batch_size = t.shape[0]
         out = vals.gather(-1, t.to(self.device)).to(self.device) # t is a tensor of shape (batch_size,), returns a tensor of shape (batch_size,) with corresponding values
 
