b644bb26be5b46c7f40ced1bab23ab16d2773aa3
Samuel Friese <samuel.friese@tum.de>
2024-10-07 14:05:07 +0200

    .

--- sample.py
+++ sample.py
@@ -16,9 +16,9 @@ parser = argparse.ArgumentParser(description="Sample from the model")
 
 parser.add_argument("--batch_size", type=int, default=4, help="Batch size for sampling, must be a square number")
 parser.add_argument("--timesteps", type=int, default=200, help="Number of timesteps")
-parser.add_argument("--guidance", type=float, default=7, help="guidance value")
-parser.add_argument("--c", type=int, default=0, help="Class to sample from")
-parser.add_argument("--model_path", type=str, default="checkpoints/checkpoint_epoch_42_0.0%_estimated_loss_0.037", help="Path to the model")
+parser.add_argument("--guidance", type=float, default=8, help="guidance value")
+parser.add_argument("--c", type=int, default=3, help="Class to sample from")
+parser.add_argument("--model_path", type=str, default="checkpoints_CIFAR10/checkpoint_epoch_6_0.0%_estimated_loss_0.037", help="Path to the model")
 parser.add_argument("--animate", action='store_true',
                         default=True, help="Animate the diffusion process, for a single sample")
 
